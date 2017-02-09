1d0cc88788831f56db48a7c881b93cb83f3aafd5
Brian Jones <bjones033@gmail.com>
2017-02-09 09:05:36 -0800

    Server persistence and security

--- static/script.js
+++ static/script.js
@@ -1,80 +1,102 @@
-var GAME_CONFIG = {
-    startingCash: 10,
-    seeds: {
-        a: {
-            name: "Alfalfa Sprouts",
-            imageSmall: "static/alfalfa.jpg",
-            imageMedium: "static/alfalfa.jpg",
-            imageLarge: "static/alfalfa.jpg",
-            buyCost: 3,
-            sellCost: 1,
-            harvestYield: 4,
-            harvestTimeSeconds: 40
-        },
-        b: {
-            name: "Broccoli",
-            imageSmall: "static/broccoli.jpg",
-            imageMedium: "static/broccoli.jpg",
-            imageLarge: "static/broccoli.jpg",
-            buyCost: 6,
-            sellCost: 1,
-            harvestYield: 9,
-            harvestTimeSeconds: 120
-        },
-        c: {
-            name: "Cabbage",
-            imageSmall: "static/cabbage.jpg",
-            imageMedium: "static/cabbage.jpg",
-            imageLarge: "static/cabbage.jpg",
-            buyCost: 12,
-            sellCost: 2,
-            harvestYield: 10,
-            harvestTimeSeconds: 300
+
+
+var GAME_CONFIG = null;
+
+// var GAME_CONFIG_JSON = JSON.stringify(GAME_CONFIG);
+
+var state = null;
+
+var getFromServer = function(endpoint, callback) {
+    var handleResponse = function(event) {
+	    var xhr = event.target;
+        if (xhr.readyState == 4) {
+           if (xhr.status == 200) {
+			    callback(JSON.parse(xhr.responseText));
+           } else if (xhr.status == 400) {
+                alert('Action not allowed.');
+           }
         }
-    }
+    };
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = handleResponse;
+    xhr.open('GET', endpoint, true);
+    xhr.send();
 };
 
-var state = {
-    cash: GAME_CONFIG.startingCash,
-    a: 0,
-    b: 0,
-    c: 0
+var sendToServer = function(endpoint, data, callback) {
+    var handleResponse = function(event) {
+	    var xhr = event.target;
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+			    callback(JSON.parse(xhr.responseText));
+            } else if (xhr.status == 400) {
+                console.log(xhr.responseText);
+                alert('Action not allowed.');
+            }
+        }
+    };
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = handleResponse;
+    //xhr.open('POST', '/save/' + game_name, true);
+    xhr.open('POST', endpoint, true);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send(JSON.stringify(data));
 };
-for (var i = 0; i <= 2; i++) {
-    for (var j = 0; j <= 2; j++) {
-        state[("plot" + i + j)] = {seedType: 0, sowTime: 0};
+
+var loadGameState = function() {
+    var gameStateLoaded = function(gameState) {
+        state = gameState;
+        reset();
     }
-};
+    console.log(window.location.hash);
+    getFromServer("/game-state/" + window.location.hash.substring(1), gameStateLoaded);
+}
+
+var saveGameState = function() {
+    var gameStateSaved = function() {
+    }
+    sendToServer("/save/" + state.slug, state, gameStateSaved);
+}
+
+
+var loadGameConfig = function() {
+    var gameConfigLoaded = function(gameConfig) {
+        GAME_CONFIG = gameConfig;
+        loadGameState();
+    }
+    getFromServer("/game-config", gameConfigLoaded);
+}
 
 var reset = function() {
-    state.cash = GAME_CONFIG.startingCash;
-    state.a = 0;
-    state.b = 0;
-    state.c = 0;
-
+    idDisplay = function(idName, displayType) {
+        var x = document.getElementById(idName);
+        x.style.display = (displayType);
+    };
     for (var i = 0; i <= 2; i++) {
         for (var j = 0; j <= 2; j++) {
-            state[("plot" + i + j)].seedType = 0;
-            state[("plot" + i + j)].sowTime = 0;
-        };
-    };
-
-    classDisplay = function(className, displayType) {
-        var x = document.getElementsByClassName(className);
-        for (var i = 0; i < x.length; i++) {
-            x[i].style.display = (displayType);
-        };
-    };
-    classDisplay("sowPlantButton", "block");
-    classDisplay("invisBlockContainer", "none");
-    classDisplay("soil", "block");
-    classDisplay("progressBar", "none");
-    classDisplay("button", "none");
-
+            if(state["plot" + i + j].seedType != 0) {
+                idDisplay("alfalfa" + i + j, "none");
+                idDisplay("broccoli" + i + j, "none");
+                idDisplay("cabbage" + i + j, "none");
+                var growingImage = document.getElementById("growing" + i + j);
+                growingImage.style.display = "block";
+                growingImage.style.backgroundImage = "url(" + GAME_CONFIG.seeds[state["plot" + i + j].seedType].imageLarge + ")";
+                idDisplay("soil" + i + j, "none");
+                idDisplay("progressBar" + i + j, "block");
+            } else {
+                idDisplay("alfalfa" + i + j, "block");
+                idDisplay("broccoli" + i + j, "block");
+                idDisplay("cabbage" + i + j, "block");
+                idDisplay("growing" + i + j, "none");
+                idDisplay("soil" + i + j, "block");
+                idDisplay("progressBar" + i + j, "none");
+            }
+        }
+    }
     document.getElementById("cash").innerHTML = "CASH: $" + state.cash;
-    document.getElementById("owneda").innerHTML = 0;
-    document.getElementById("ownedb").innerHTML = 0;
-    document.getElementById("ownedc").innerHTML = 0;
+    document.getElementById("owneda").innerHTML = state.a;
+    document.getElementById("ownedb").innerHTML = state.b;
+    document.getElementById("ownedc").innerHTML = state.c;
     document.getElementById("buyA").innerHTML = "$" + GAME_CONFIG.seeds.a.buyCost;
     document.getElementById("buyB").innerHTML = "$" + GAME_CONFIG.seeds.b.buyCost;
     document.getElementById("buyC").innerHTML = "$" + GAME_CONFIG.seeds.c.buyCost;
@@ -85,7 +107,7 @@ var reset = function() {
 
 document.onreadystatechange = function() {
   if (document.readyState === 'complete') {
-    reset();
+    loadGameConfig();
   }
 };
 
@@ -98,6 +120,7 @@ var buy = function(seed) {
     document.getElementById("owned" + seed).innerHTML = state[seed];
     state.cash -= GAME_CONFIG.seeds[seed].buyCost;
     document.getElementById("cash").innerHTML = "CASH: $" + state.cash;
+    saveGameState();
 };
 
 var sell = function(seed) {
@@ -109,6 +132,7 @@ var sell = function(seed) {
     document.getElementById("owned" + seed).innerHTML = state[seed];
     state.cash += GAME_CONFIG.seeds[seed].sellCost;
     document.getElementById("cash").innerHTML = "CASH: $" + state.cash;
+    saveGameState();
 };
 
 var sow = function(x, y, seed) {
@@ -133,6 +157,7 @@ var sow = function(x, y, seed) {
     tick();
     var progressBar = document.getElementById("progressBar" + x + y);
     progressBar.style.display = "block";
+    saveGameState();
 };
 
 var harvest = function(x, y) {
@@ -151,10 +176,14 @@ var harvest = function(x, y) {
     soil.style.display = "block";
     var button = document.getElementById("button" + x + y);
     button.style.display = "none";
-    seed = 0;
+    state["plot" + x + y].seedType = 0;
+    saveGameState();
 };
 
 var tick = function() {
+    if (state == null) {
+        return;
+    }
     for (var i = 0; i <= 2; i++) {
         for (var j = 0; j <= 2; j++) {
             if(state["plot" + i + j].seedType != 0) {
@@ -171,10 +200,11 @@ var tick = function() {
                     clockDisplay = '0' + Math.floor(countdown / 60) + ":0" + countdown % 60;
                 }
                 document.getElementById("time" + i + j).innerHTML = clockDisplay;
-                if (countdown == 0) {
+                if (countdown <= 0) {
                     document.getElementById("progressBar" + i + j).style.display = "none";
                     document.getElementById("button" + i + j).style.display = "block";
                 }
+
 
             };
         };
--- webserver.py
+++ webserver.py
@@ -1,11 +1,91 @@
 #!usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import json, os, re
 
 app = Flask(__name__)
 
-@app.route('/game')
+@app.route('/game/')
 def default():
     return render_template('frontEndLayout.html')
+
+
+
+@app.route('/game-config')
+def game_config():
+    return jsonify({
+            'startingCash': 10,
+            'seeds': {
+                'a': {
+                    'name': "Alfalfa Sprouts",
+                    'imageSmall': "/static/alfalfa_s.jpg",
+                    'imageMedium': "/static/alfalfa_m.jpg",
+                    'imageLarge': "/static/alfalfa_l.jpg",
+                    'buyCost': 3,
+                    'sellCost': 1,
+                    'harvestYield': 4,
+                    'harvestTimeSeconds': 40
+                },
+                'b': {
+                    'name': "Broccoli",
+                    'imageSmall': "/static/broccoli_s.jpg",
+                    'imageMedium': "/static/broccoli_m.jpg",
+                    'imageLarge': "/static/broccoli_l.jpg",
+                    'buyCost': 6,
+                    'sellCost': 1,
+                    'harvestYield': 9,
+                    'harvestTimeSeconds': 120
+                },
+                'c': {
+                    'name': "Cabbage",
+                    'imageSmall': "/static/cabbage_s.jpg",
+                    'imageMedium': "/static/cabbage_m.jpg",
+                    'imageLarge': "/static/cabbage_l.jpg",
+                    'buyCost': 12,
+                    'sellCost': 2,
+                    'harvestYield': 10,
+                    'harvestTimeSeconds': 10000
+                }
+            }
+        })
+
+@app.route('/game-state/<slug>')
+def game_state(slug):
+    regEx = re.compile("^[a-z]+$")
+    x = regEx.match(slug)
+    if x is False:
+        raise Exception("Invalid characters")
+
+
+    if os.path.exists('saves/' + slug + '.json'):
+        with open('saves/' + slug + '.json', 'r') as file:
+            data = json.load(file)
+        return jsonify(data)
+    else:
+
+        game_state = {'cash': 10,
+            'slug' : slug,
+            'a': 0,
+            'b': 0,
+            'c': 0, }
+
+        for i in range(3):
+            for j in range(3):
+                game_state[("plot" + str(i) + str(j))] = {'seedType': 0, 'sowTime': 0}
+        with open('saves/' + slug + '.json', 'w') as file:
+            json.dump(game_state, file, indent = 4)
+            return jsonify(game_state)
+
+@app.route('/save/<slug>', methods = ['GET', 'POST'])
+def save_game_state(slug):
+    regEx = re.compile("^[a-z]+$")
+    x = regEx.match(slug)
+    if x is False:
+        raise Exception("Invalid characters")
+
+    game_state = request.json
+    with open('saves/' + slug + '.json', 'w') as file:
+        json.dump(game_state, file, indent = 4)
+    return jsonify({})
 
 app.run(debug=True,port=8000)

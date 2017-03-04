528cbc7058ce02cea9a291bffeea7a969b458751
Parker Jones <parkerhbljones@gmail.com>
2017-03-03 16:49:49 -0800

    more helpful test output

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,6 +9,6 @@ class TestConfig(unittest.TestCase):
         self.assertTrue('seeds' in webserver.GAME_CONFIG)
         for seed in webserver.GAME_CONFIG['seeds']:
             for data in seed_data:
-                self.assertTrue(data in webserver.GAME_CONFIG['seeds'][seed])
+                self.assertTrue(data in webserver.GAME_CONFIG['seeds'][seed], "%s not in %s" % (data, webserver.GAME_CONFIG['seeds'][seed]))
         self.assertTrue('starting_resources' in webserver.GAME_CONFIG)
         self.assertTrue('cash' in webserver.GAME_CONFIG['starting_resources'])

3e1ee000ce29fd9435b9bfa99cd49d6783d116cd
Brian Jones <bjones033@gmail.com>
2017-03-03 17:12:58 -0800

    Unittest update

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,7 +5,7 @@ class TestConfig(unittest.TestCase):
 
     def test_config_data(self):
         seed_data = ['name', 'imageSmall', 'imageMedium', 'imageLarge',
-                     'buyCost', 'sellCost', 'seedYield', 'cashYield', 'harvestTimeSeconds']
+                     'sellCost', 'seedYield', 'cashYield', 'harvestTimeSeconds']
         self.assertTrue('seeds' in webserver.GAME_CONFIG)
         for seed in webserver.GAME_CONFIG['seeds']:
             for data in seed_data:
